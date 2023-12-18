cb85a809db7ebabdcdf8694987ccbcfe25930c70
deawid12 <dawid_plocharczyk3@o2.pl>
2023-12-18 18:31:08 +0100

    Initial commit

--- mysite/polls/serializers.py
+++ mysite/polls/serializers.py
@@ -1,34 +1,54 @@
 from rest_framework import serializers
-from .models import TwojModel
+from .models import Osoba, Stanowisko, MONTHS, SHIRT_SIZES
 from datetime import datetime
 
-class TwojModelSerializer(serializers.Serializer):
+class PersonSerializer(serializers.Serializer):
+    class Meta:
+        model = Osoba
+        fields = '__all__'
+
     id = serializers.IntegerField(read_only=True)
     nazwa = serializers.CharField(max_length=100)
     pole1 = serializers.CharField(max_length=100)
     pole2 = serializers.IntegerField()
-    data_dodania = serializers.DateField()
-
+    shirt_size = serializers.ChoiceField(choices=SHIRT_SIZES, default=SHIRT_SIZES[0][0])
+    miesiac_dodania = serializers.ChoiceField(choices=MONTHS.choices, default=MONTHS.choices[0][0])
+    stanowisko = serializers.PrimaryKeyRelatedField(queryset=Stanowisko.objects.all())
     def validate_nazwa(self, value):
-        # Walidacja dla pola nazwa - może zawierać tylko litery
         if not value.isalpha():
             raise serializers.ValidationError("Nazwa może zawierać tylko litery.")
         return value
 
     def validate_data_dodania(self, value):
-        # Walidacja dla pola data_dodania
         if value > datetime.now().date():
             raise serializers.ValidationError("Data dodania nie może być z przyszłości.")
         return value
 
     def create(self, validated_data):
         validated_data['data_dodania'] = validated_data.get('data_dodania', datetime.now().date())
-        return TwojModel.objects.create(**validated_data)
+        return Osoba.objects.create(**validated_data)
 
     def update(self, instance, validated_data):
-        instance.nazwa = validated_data.get('nazwa', instance.nazwa)
-        instance.pole1 = validated_data.get('pole1', instance.pole1)
-        instance.pole2 = validated_data.get('pole2', instance.pole2)
-        instance.data_dodania = validated_data.get('data_dodania', instance.data_dodania)
+        instance.name = validated_data.get('name', instance.name)
+        instance.shirt_size = validated_data.get('shirt_size', instance.shirt_size)
+        instance.miesiac_dodania = validated_data.get('miesiac_dodania', instance.miesiac_dodania)
+        instance.team = validated_data.get('team', instance.team)
         instance.save()
         return instance
+
+    def validate_name(self, value):
+        if not value.istitle():
+            raise serializers.ValidationError("Imię powinno zaczynać się od wielkiej litery.")
+        return value
+
+    def update(self, instance, validated_data):
+        instance.name = validated_data.get('name', instance.name)
+        instance.shirt_size = validated_data.get('shirt_size', instance.shirt_size)
+        instance.data_dodanie = validated_data.get('data_dodania', instance.miesiac_dodania)
+        instance.stanowisko = validated_data.get('stanowisko', instance.team)
+        instance.save()
+        return instance
+class StanowiskoSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Stanowisko
+        fields = '__all__'
---
+++ mysite/polls/urls.py
@@ -0,0 +1,20 @@
+# from django.contrib import admin
+# from django.urls import path, include
+#
+#
+# urlpatterns = [
+#     path('admin/', admin.site.urls),
+#     path('admin-tools/', include('admin_tools.urls'),),
+#     path('mysite/', include('mysite.urls'),)
+# ]
+
+from django.urls import path
+from .views import index, person_list, person_detail
+
+urlpatterns = [
+    path('', index, name='index'),
+    path('osoby/', person_list, name='person-list'),
+    path('osoby/<int:pk>/', person_detail, name='person-detail'),
+]
+
+
